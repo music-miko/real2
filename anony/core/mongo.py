b156b17876234aa0263d8fac7e5052248df1c501
music-miko <voilet856@gmail.com>
2025-12-06 14:13:31 +0530

    Update mongo.py
    Signed-off-by: music-miko <voilet856@gmail.com>

--- anony/core/mongo.py
+++ anony/core/mongo.py
@@ -17,7 +17,7 @@ class MongoDB:
         Initialize the MongoDB connection.
         """
         self.mongo = AsyncMongoClient(config.MONGO_URL, serverSelectionTimeoutMS=12500)
-        self.db = self.mongo.arc
+        self.db = self.mongo.MusicBot
 
         self.admin_list = {}
         self.active_calls = {}
