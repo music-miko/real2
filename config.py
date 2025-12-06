a8d6bc810cc93f6609f5c85cbc92a19ebdd8149b
music-miko <voilet856@gmail.com>
2025-12-06 12:03:28 +0530

    Update config.py
    Signed-off-by: music-miko <voilet856@gmail.com>

--- config.py
+++ config.py
@@ -22,8 +22,12 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+        self.API_KEY = getenv("API_KEY")
+        self.API_URL = getenv("API_URL")
+
+        
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/arcupdates")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ArcChatz")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
@@ -33,8 +37,8 @@ class Config:
             if url and "batbin.me" in url
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/a0zkhc.jpg")
+        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/a0zkhc.jpg")
 
     def check(self):
         missing = [
