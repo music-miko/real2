4406cb500f2cdb54fde63264f16e67f23aeff870
music-miko <voilet856@gmail.com>
2025-12-11 23:45:27 +0530

    Update fallenapi.py
    Signed-off-by: music-miko <voilet856@gmail.com>

--- anony/core/fallenapi.py
+++ anony/core/fallenapi.py
@@ -63,8 +63,8 @@ class FallenApi:
         job_poll_attempts: int = 20,
         job_poll_interval: float = 2.0,
     ):
-        self.api_url = config.API_URL.rstrip("/")
-        self.api_key = config.API_KEY
+        self.api_url = Config.API_URL.rstrip("/")
+        self.api_key = Config.API_KEY
         self.retries = retries
         self.timeout = aiohttp.ClientTimeout(total=timeout)
         self.download_dir = Path(download_dir)
